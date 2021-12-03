a36b355706394d025478aca9c117380b8c256bad
nick-456 <94133116+nick-456@users.noreply.github.com>
2021-12-03 14:35:10 +0900

    fsd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import "./styles.css";
 import { Home } from "./Home";
