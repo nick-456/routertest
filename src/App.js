08a704b19621a96ce51b99796e5db5e35233afb6
nick-456 <94133116+nick-456@users.noreply.github.com>
2021-12-06 13:05:20 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { BrowserRouter, Link } from "react-router-dom";
-import { Router } from "./router/Router";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
 import "./styles.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 export default function App() {
   return (
@@ -13,7 +15,17 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Router />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
